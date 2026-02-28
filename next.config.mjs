69c614265f34f8b6ae1ae8cee4284512edef0e41
Alexis <pokemapler@gmail.com>
2026-02-28 16:54:57 -0600

    v1.21

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Esto genera la carpeta /out con archivos HTML/CSS/JS
+  images: {
+    unoptimized: true, 
+  },
 };
 
 module.exports = nextConfig;
